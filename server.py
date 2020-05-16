9eab4bb154f054522f961941f1a83ed7334610ee
mridul <mriduladvani@gmail.com>
2020-05-16 15:41:48 -0400

    client and server basic functionality

--- server.py
+++ server.py
@@ -3,13 +3,15 @@ HOST = '127.0.0.1'
 PORT = 9000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected By:', addr)
-        while True:
-            data=conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    s.bind((HOST, PORT)) #bind the socket with network interface and port
+
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected By:', addr)
+            while True:
+                data=conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
