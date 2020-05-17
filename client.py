b9d5bf5e4de23d76e1a99a6c2cf77d919c29ebfb
mridul <mriduladvani@gmail.com>
2020-05-16 23:19:50 -0400

    Client menu added

--- client.py
+++ client.py
@@ -5,7 +5,25 @@ PORT= 9000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, World')
-    data= s.recv(1024)
+    while True:
+        print("Python DB Menu")
+        print("1. Find Customer")
+        print("2. Add Customer")
+        print("3. Delete Customer")
+        print("4. Update Customer age")
+        print("5. Update Customer address")
+        print("6. Update Customer phone")
+        print("7. Print Report")
+        print("8. Exit")
+        selection= input("Select: ")
+        if selection== "1":
+            name= input("Enter the name")
+            request_message= selection + "-" + name
+            s.sendall(request_message.encode())
+            data= s.recv(1024)
+            print(data.decode())
+            continue
+        if selection=="8":
+            break;
+        data= s.recv(1024)
 
-print('Received', repr(data))
--- server.py
+++ server.py
@@ -3,15 +3,27 @@ HOST = '127.0.0.1'
 PORT = 9000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT)) #bind the socket with network interface and port
+    s.bind((HOST, PORT))
+    dictionary={}
+    name= "John"
+    file= open("data.txt", "r")
+    for x in file:
+        y= x.split("|", 1)
+        dictionary[y[0]] = y[1];
 
     while True:
         s.listen()
         conn, addr = s.accept()
+
+
+
+
         with conn:
-            print('Connected By:', addr)
             while True:
                 data=conn.recv(1024)
+                if data.decode().split("-")[0]=="1":
+                    name= data.decode().split("-")[1]
+                    if name in dictionary:
+                        conn.sendall((name + " " + " ".join(dictionary[name].split("|"))).encode())
                 if not data:
                     break
-                conn.sendall(data)
